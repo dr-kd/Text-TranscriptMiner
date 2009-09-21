099233a0c17497f25dc8e5d2a56c0d859d00a3df
Kieren Diment <kd@bld39-ach-172-30-17-41.wlan.uow.edu.au>
2009-09-21 13:27:31 +1000

    documentation

--- lib/Text/TranscriptMiner/Document.pm
+++ lib/Text/TranscriptMiner/Document.pm
@@ -8,6 +8,13 @@ has 'filename' =>  (isa => 'Str', is => 'rw');
 has 'file'     =>  (isa => 'Path::Class::File', is => 'rw');
 has 'info'     =>  (isa => 'HashRef', is => 'ro', lazy_build => 1);
 
+=head2 _build_info()
+
+Returns the metadata embedded in html style comment tags in the file as a
+hashref.
+
+=cut
+
 sub _build_info {
     my ($self) = @_;
     my ($metatxt) = $self->txt =~ /<!--(.*)-->/ms;
@@ -25,6 +32,11 @@ sub _build_info {
     return \%metadata;
 }
 
+=head2 sub BUILD
+
+Ensures that you either set up the object with the C<file> or C<txt> attribute, but not both, or neither.
+
+=cut
 
 sub BUILD {
     my ($self) = @_;
@@ -42,6 +54,14 @@ sub BUILD {
     $self->txt($txt);
 }
 
+=head2 sub get_tagged_txt($tag, [$txt])
+
+Returns an array reference containing the tagged text (one array entry per instance of that tag) from either C<<$self->txt>> or optionally the passed in
+C<$txt> (which is intended to be a subset of C<<$self->txt>>, but could be
+anything).
+
+=cut
+
 sub get_tagged_txt {
     my ($self, $tag, $txt) = @_;
     croak "Need to supply a tag" if ! $tag;
@@ -56,6 +76,13 @@ sub get_tagged_txt {
     }
 }
 
+=head2 sub get_all_tags([$txt])
+
+Returns a hashref keyed by the tag label, with the number of occurences of that
+tag as the value
+
+=cut
+
 sub get_all_tags {
     my ($self, $txt) = @_;
     $txt = $self->txt if ! $txt;
@@ -64,6 +91,13 @@ sub get_all_tags {
     $tagged_txt{$_}++ for @tagged_txt;
     return \%tagged_txt;
 }
+
+=head2 sub get_this_tag($tag, [$txt])
+
+Worker sub for L<get_tagged_txt> which returns the arrayref described in the
+documentation for L<get_tagged_txt>.
+
+=cut
 
 sub get_this_tag {
     my ($self, $tag, $txt) = @_;
--- lib/Text/TranscriptMiner/Document/Interview.pm
+++ lib/Text/TranscriptMiner/Document/Interview.pm
@@ -2,3 +2,11 @@ package Text::TranscriptMiner::Document::Interview;
 use Moose;
 extends 'Text::TranscriptMiner::Document';
 
+=head1 Text::TranscriptMinder::Document::Interview
+
+Subclass of Text::TranscriptMiner::Document specifically for interview
+transcripts.  Doesn't do anything yet.
+
+=cut
+
+1;
